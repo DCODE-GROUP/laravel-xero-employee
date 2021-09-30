031e87e29107209de3794369ff4b444da259dff0
lionslair <lionslair@users.noreply.github.com>
2021-09-30 07:57:06 +0000

    Fix styling

--- src/LaravelXeroEmployeeServiceProvider.php
+++ src/LaravelXeroEmployeeServiceProvider.php
@@ -6,14 +6,11 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelXeroEmployeeServiceProvider extends ServiceProvider
 {
-
     public function boot()
     {
-
     }
 
     public function register()
     {
-
     }
 }
