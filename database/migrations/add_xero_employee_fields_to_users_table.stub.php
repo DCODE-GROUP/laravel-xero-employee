1b970559e149a5d931c0b8784ab61d2738cf5478
Nathan Rzepecki <nathan@lionslair.net.au>
2021-10-01 13:53:13 +0800

    Update the migration stub

--- database/migrations/add_xero_employee_fields_to_users_table.stub.php
+++ database/migrations/add_xero_employee_fields_to_users_table.stub.php
@@ -16,6 +16,7 @@ class AddXeroEmployeeFieldsToUsersTable extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->after('password', function ($table) {
                 $table->string('xero_employee_id', 50)->nullable();
+                $table->string('xero_default_payroll_calendar_id', 50)->nullable();
                 $table->string('xero_default_earnings_rate_id', 50)->nullable();
                 $table->string('xero_time_and_a_half_earnings_rate_id', 50)->nullable();
                 $table->string('xero_double_time_earnings_rate_id', 50)->nullable();
