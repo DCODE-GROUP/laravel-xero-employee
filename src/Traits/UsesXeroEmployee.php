6f35e37174cc971c5333dbb5b2cd81263510bafb
Nathan Rzepecki <nathan@lionslair.net.au>
2021-10-05 16:05:57 +0800

    Make sure all the  xero fields are populated

--- src/Traits/UsesXeroEmployee.php
+++ src/Traits/UsesXeroEmployee.php
@@ -10,4 +10,12 @@ trait UsesXeroEmployee
     {
         return $query->whereNull('xero_employee_id');
     }
+
+    public function isValidXeroEmployee(): bool
+    {
+        return $this->xero_employee_id
+            && $this->xero_default_earnings_rate_id
+            && $this->xero_time_and_a_half_earnings_rate_id
+            && $this->xero_double_time_earnings_rate_id;
+    }
 }
