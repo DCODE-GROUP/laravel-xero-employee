4c938b8daa6ecc2597b49a18d7952b77697f5c5b
Nathan Rzepecki <nathan@lionslair.net.au>
2023-12-19 08:52:02 +0800

    clear the employee id if not found

--- src/BaseXeroEmployeeService.php
+++ src/BaseXeroEmployeeService.php
@@ -44,6 +44,16 @@ class BaseXeroEmployeeService extends BaseXeroService
                     'xero_employee_id' => null,
                 ]);
             }
+        } catch (\XeroPHP\Remote\Exception\NotFoundException $e) {
+            if (! app()->isProduction()) {
+                // not found so clear what is stored
+                logger('employee not found with email: '.$user->email);
+                $user->update([
+                    'xero_employee_id' => null,
+                ]);
+            }
+
+            return false;
         } catch (Exception $e) {
             Log::error('Employee not found in xero syncXeroEmployee error: '.$e->getMessage());
             report($e);
